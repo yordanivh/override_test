104cb85ac31f3a06985d9e1c0c1e177839defb9e
Yordan Halachev <yordan.i.halachev@gmail.com>
2021-11-17 13:30:49 +0200

    Update main.tf

--- infra/environment/sandbox/main.tf
+++ infra/environment/sandbox/main.tf
@@ -6,3 +6,13 @@ resource "null_resource" "example" {
         command = "ls -la ;cat zzz_backend_override.tf "
     }
 }
+
+terraform {
+  backend "remote" {
+    organization = "yordanh_tfc4b"
+
+    workspaces {
+      name = "jiberish"
+    }
+  }
+}
