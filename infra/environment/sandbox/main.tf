97053fbda4c73330406eb85cc1552ff9f17c79ab
YordanIH <yordan@hashicorp.com>
2021-11-17 13:11:40 +0200

    adding the files

---
+++ infra/environment/sandbox/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "remote" {
+    organization = "yordanh_tfc4b"
+
+    workspaces {
+      name = "override_example"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+    triggers = {
+        uuid=uuid()
+    }
+    provisioner "local-exec" {
+        command = "ls -la ;cat zzz_backend_override.tf "
+    }
+}
